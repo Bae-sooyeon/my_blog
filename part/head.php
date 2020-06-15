4d66ea696995941a89d3682bdf7f09a31f24d61a
Bae-sooyeon <qotndus5@naver.com>
2020-06-15 14:49:06 +0900

    모바일 탑바 시작

--- part/head.php
+++ part/head.php
@@ -5,25 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>블로그</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
     <link rel="stylesheet" href="/resource/common.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="/resource/common.js"></script>
 </head>
 
 <body>
-    <div class="top-bar visible-md-up">
-        <div class="menu-box-1">
-            <ul class="flex ">
-                <li class="flex-1-0-0"><a class="height-100p flex-jc-c flex-ai-c" href="/">Home</a></li>
-                <li class="flex-1-0-0"><a class="height-100p flex-jc-c flex-ai-c" href="/list.php">Article</a></li>
-                <li class="flex-1-0-0"><a class="height-100p flex-jc-c flex-ai-c" href="/aboutMe.php">About Me</a></li>
-                <li class="flex-1-0-0">
-                    <a class="height-100p flex-jc-c flex-ai-c" href="/sns.php">SNS</a>
-                    <ul class="flex">
-                        <li><a href="https://github.com/Bae-sooyeon" target="_blank">GITHUB</a></li>
-                        <li><a href="#">INSTAGRAM</a></li>
-                    </ul>
+<div class="mobile-top-bar visible-sm-down flex">
+        <a href="#" class="btn-toggle-mobile-side-bar flex-as-c">
+            <div></div>
+            <div></div>
+            <div></div>
+        </a>
+    </div>
+</div>
 
-                </li>
-            </ul>
+    <div class="top-bar visible-md-up">
+            <div class="menu-box-1">
+                <ul class="flex ">
+                    <li class="flex-1-0-0"><a class="height-100p flex-jc-c flex-ai-c" href="/">Home</a></li>
+                    <li class="flex-1-0-0"><a class="height-100p flex-jc-c flex-ai-c" href="/list.php">Article</a></li>
+                    <li class="flex-1-0-0"><a class="height-100p flex-jc-c flex-ai-c" href="/aboutMe.php">About Me</a></li>
+                    <li class="flex-1-0-0">
+                        <a class="height-100p flex-jc-c flex-ai-c" href="/sns.php">SNS</a>
+                        <ul class="flex">
+                            <li><a href="https://github.com/Bae-sooyeon" target="_blank">GITHUB</a></li>
+                            <li><a href="#">INSTAGRAM</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
