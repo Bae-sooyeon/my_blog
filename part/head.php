fcb3ddd134c6b5453d21391595a14ea491a3eb3e
Bae-sooyeon <qotndus5@naver.com>
2020-06-18 18:25:47 +0900

    로고 넣음

--- part/head.php
+++ part/head.php
@@ -22,44 +22,97 @@
             <li><a href="#">Article</a></li>
             <li><a href="#">About Me</a></li>
             <li><a href="#">SNS</a>
-            <ul>
+                <ul>
                     <li><a href="https://github.com/Bae-sooyeon" target="_blank">GITHUB</a></li>
-                    <li><a href="https://www.youtube.com/channel/UCin7a7Q-f67ZjD87GCPxkqw?view_as=subscriber" target="_blank">YOUTUBE</a></li>
+                    <li><a href="https://www.youtube.com/channel/UCin7a7Q-f67ZjD87GCPxkqw?view_as=subscriber"
+                            target="_blank">YOUTUBE</a></li>
                     <li><a href="#">INSTAGRAM</a></li>
-                </ul></li>
-                
+                </ul>
+            </li>
+
         </ul>
     </div>
 </div>
 
 <!-- 모바일 탑바 -->
+
 <body>
-<div class="mobile-top-bar visible-sm-down flex">
+    <div class="mobile-top-bar visible-sm-down flex">
         <a href="#" onclick="return false;" class="btn-toggle-mobile-side-bar flex-as-c">
             <div></div>
             <div></div>
             <div></div>
         </a>
     </div>
-</div>
+    </div>
 
 
+    <!-- 탑바 -->
 
+    <div class="top-line"></div>
     <div class="top-bar visible-md-up">
-            <div class="menu-box-1">
-                <ul class="flex ">
-                    <li class="flex-1-0-0"><a class="height-100p flex-jc-c flex-ai-c" href="/">Home</a></li>
-                    <li class="flex-1-0-0"><a class="height-100p flex-jc-c flex-ai-c" href="/list.php">Article</a></li>
-                    <li class="flex-1-0-0"><a class="height-100p flex-jc-c flex-ai-c" href="/aboutMe.php">About Me</a></li>
-                    <li class="flex-1-0-0">
-                        <a class="height-100p flex-jc-c flex-ai-c" href="/sns.php">SNS</a>
-                         <ul class="flex">
-                            <li><a href="https://github.com/Bae-sooyeon" target="_blank">GITHUB</a></li>
-                            <li><a href="https://www.youtube.com/channel/UCin7a7Q-f67ZjD87GCPxkqw?view_as=subscriber" target="_blank">YOUTUBE</a></li>
-                            <li><a href="#">INSTAGRAM</a></li>
-                         </ul>
-                    </li>
-                </ul>
+        <div class="menu-box-1 flex flex-space-bt">
+            <div class="logo">
+                <svg version="1.1" class="logo-img" xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1417.32 1417.32"
+                    style="enable-background:new 0 0 1417.32 1417.32;" xml:space="preserve">
+                    <style type="text/css">
+                        .st0 {
+                            stroke-miterlimit: 10;
+                        }
+
+                        .st1 {
+                            fill: none;
+                            stroke-width: 23;
+                            stroke-linejoin: round;
+                            stroke-miterlimit: 10;
+                        }
+
+                        .st2 {
+                            stroke-width: 8;
+                            stroke-miterlimit: 10;
+                        }
+                    </style>
+                    <polyline class="st0"
+                        points="434.46,490.65 417.27,416.32 399.48,339.43 364.06,426.68 470.61,405.92 " />
+                    <path class="st1"
+                        d="M569.23,345.1c0,0,264.77,38.32,344.9,378.58L479.81,836.32C479.81,836.32,667.94,624.97,569.23,345.1z" />
+                    <path class="st1" d="M569.23,345.1c0,0,135.83,150.64,99.27,442.29" />
+                    <path class="st1"
+                        d="M881.61,975.68c135.87-105.68,89.42-182.32,89.42-182.32L438,895.55c0,0-2.32,53.42,67.35,88.26" />
+                    <path class="st1"
+                        d="M390.75,977.83c17.14-15.92,41.25-23.89,64.49-21.08c35.68,4.31,55.91,46.82,92.49,41.37
+                    c21.04-3.13,34.08-19.74,49.46-32.6c19.21-16.07,43.84-14.07,65.03-1.73c22.77,13.26,38.88,42.95,65.97,48.11
+                    c38.18,7.27,50.92-36.91,75.64-54.73c12.19-8.78,28.7-11.24,42.92-6.38c12.96,4.43,23.31,14.2,32.7,24.16
+                    c9.39,9.96,18.48,20.58,30.26,27.55c56.12,33.19,70.82-61.15,122.5-47.8c13.42,3.47,24.76,12.23,35.43,21.07" />
+                    <path class="st1" d="M447.17,634.65c-29.42,0-40.26-32.79-7.74-80.09s-22.1-79.47-22.1-79.47" />
+                    <polygon class="st1" points="470.61,405.92 434.46,490.65 364.06,426.68 399.48,339.43 " />
+                    <path class="st1" d="M402.52,352.56" />
+                    <path class="st2" d="M396.79,420.3" />
+                    <g>
+                        <polygon points="399.48,339.43 364.06,426.68 417.27,416.32 403.71,357.69 399.48,339.43 	" />
+                    </g>
+                    <g>
+                        <polygon points="470.61,405.92 417.27,416.32 430.42,473.19 470.61,405.92 	" />
+                    </g>
+                </svg>
             </div>
+            
+            <ul class="flex-jc-end ">
+                <li class=""><a class="height-100p flex-jc-c flex-ai-c" href="/">Home</a></li>
+                <li class=""><a class="height-100p flex-jc-c flex-ai-c" href="/list.php">Article</a></li>
+                <li class=""><a class="height-100p flex-jc-c flex-ai-c" href="/aboutMe.php">About Me</a></li>
+                <li class="">
+                    <a class="height-100p flex-jc-c flex-ai-c" href="/sns.php">SNS</a>
+                    <ul class="flex">
+                        <li><a href="https://github.com/Bae-sooyeon" target="_blank">GITHUB</a></li>
+                        <li><a href="https://www.youtube.com/channel/UCin7a7Q-f67ZjD87GCPxkqw?view_as=subscriber"
+                                target="_blank">YOUTUBE</a></li>
+                        <li><a href="#">INSTAGRAM</a></li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     </div>
+    </div>
+    <div class="background"></div>
--- public/index.php
+++ public/index.php
@@ -3,26 +3,7 @@ include "../part/head.php"
 ?>
 
 <h1>홈</h1>
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore dolore ab pariatur, debitis corporis nihil doloremque nisi velit. Ullam, debitis corrupti. Labore molestiae, minus culpa odio numquam vero neque dolore!
-Sint, impedit reprehenderit, ab odio necessitatibus voluptas molestias sit, quia voluptate suscipit quod. Tempore quis quidem voluptate eius illum unde autem excepturi enim molestias, architecto voluptas aspernatur, culpa error quia.
-Vitae nulla quo minus a cum, reprehenderit quod molestias delectus velit architecto temporibus, tenetur, quaerat numquam alias eveniet laboriosam voluptas ratione in necessitatibus fuga. Temporibus iste natus distinctio. Nostrum, sapiente.
-Voluptatem accusamus consequatur officia odit molestiae minus sequi quisquam hic doloremque! Nihil excepturi in quam, temporibus laborum praesentium vel dolore dolorem odit libero cumque consequatur eveniet, nostrum, assumenda eos facilis.
-Soluta consectetur vitae quo accusamus cumque enim quisquam architecto vero, quidem quasi non alias fuga mollitia amet, consequatur doloremque ipsa totam aspernatur doloribus obcaecati aperiam saepe modi nisi cupiditate? Suscipit?
-Autem explicabo doloribus assumenda, at eligendi aut laborum sapiente rerum fugiat sunt, corporis error consequatur officiis ratione tenetur, ab consequuntur accusantium? Ut odit ducimus, maiores tempora exercitationem explicabo repellat eligendi.
-Reprehenderit, blanditiis recusandae. Eveniet nobis quibusdam tempora, labore voluptatem vel ut libero voluptas suscipit dolores, dolorum consectetur ratione quo officia! Iusto dolorum excepturi ab consequatur quod obcaecati, alias sit dignissimos.
-Voluptatem quo quidem iure in incidunt! Eius, repudiandae velit! Quis iure, eum atque quaerat veritatis corporis pariatur ex id ipsam maxime optio! Harum deserunt hic repellendus eligendi accusamus officiis sunt.
-Quas sequi maiores enim nostrum dolorem commodi voluptas sit eius tempore, officia vero velit! Amet hic numquam voluptates est quasi saepe, ex, beatae distinctio minus obcaecati sit nostrum facere molestias!
-Magnam voluptatum culpa dolorem ducimus, praesentium qui minima sapiente delectus nisi sunt voluptate esse pariatur harum, architecto beatae iste fugit eveniet ullam. Error, sint ipsa sapiente consectetur quibusdam blanditiis in!
-Quam commodi fugit accusantium quia amet in explicabo! Laborum amet, odit quis dolores aut maxime. Minus assumenda, voluptatum neque eius consectetur dolorem sed ea accusantium. Dicta delectus eaque ea assumenda?
-Dolor pariatur repudiandae, nostrum quas aperiam ullam obcaecati iure mollitia rerum unde magnam repellendus earum possimus tempore cupiditate excepturi. Quae non similique sequi error? Temporibus nesciunt exercitationem itaque distinctio asperiores?
-Assumenda nam quod commodi ipsa porro alias qui libero deserunt perspiciatis unde facilis, maxime in tempora quo tenetur aliquid velit explicabo accusamus placeat quibusdam dignissimos sit, impedit natus? Nulla, nam.
-Praesentium architecto quibusdam optio maxime facilis voluptatum laborum excepturi quo magni ipsa. Quam error, debitis eligendi aut ullam omnis sunt, vel repudiandae explicabo recusandae itaque iste ab doloribus! Cumque, fuga.
-Voluptates odio sunt eaque dolores omnis maxime accusantium iste animi eligendi, ipsam possimus, provident labore blanditiis nulla temporibus, voluptatem aperiam quos voluptatum esse odit eum assumenda corrupti repellendus! Provident, aspernatur!
-Alias corrupti sit vitae cum veritatis nobis, quia esse corporis facilis, voluptatibus dolore reiciendis debitis quis officiis expedita maxime impedit doloremque omnis hic molestias. Repellendus nisi est laborum officia voluptatibus.
-Consectetur nisi maxime quis quo magnam. Cumque corrupti fuga laboriosam. Magni nesciunt distinctio neque quidem ea eum sit id magnam ullam eveniet. Harum beatae possimus labore similique adipisci unde enim.
-Ad repellat tenetur accusantium minus, debitis excepturi nesciunt, cupiditate recusandae minima officiis delectus enim dolorem possimus qui rem hic distinctio quis odit modi maxime labore consequatur nihil repellendus. Totam, illo!
-Debitis tempora temporibus earum sed, quisquam asperiores eveniet delectus id aut ad quia beatae dolores similique enim illo harum, dolor, eum repellendus placeat necessitatibus magnam recusandae! Iusto, ex ea? Doloribus.
-Dolorem animi explicabo sint molestiae deserunt, voluptates ullam beatae suscipit harum, reiciendis ad magnam, commodi libero! Culpa, odio repellendus sint necessitatibus fuga voluptatum asperiores, eaque, provident dignissimos illo labore distinctio!
+
 
 <?php
 include "../part/foot.php"
