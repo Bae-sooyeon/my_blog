ce250298cdcd39290d2fd1e442d51d0e40ed1099
Bae-sooyeon <qotndus5@naver.com>
2020-06-30 23:30:40 +0900

    리스트 약간 수정

--- public/list.php
+++ public/list.php
@@ -5,8 +5,24 @@ include "../part/head.php"
 
 <div class="list-box-1 con">
     <ul>
-        <li><a href="detail.php?id=2">크로스 브라우징과 벤더 프리픽스</a></li>
-        <li><a href="detail.php?id=2">도메인과 URL, 포트</a></li>
+        <li>
+            <a href="detail.php?id=1">
+                <div class="img-bo flex ">
+                    <img src="/resource/img/list-1.jpg" alt="" width="200">
+                    <div class="title flex">크로스 브라우징과 벤더 프리픽스</div>
+                </div>
+            </a>
+        </li>
+
+        <li>
+            <a href="detail.php?id=2">
+                <div class="img-bo flex">
+                    <img src="/resource/img/list-2.jpg" alt="" width="200">
+                    <div class="title">도메인과 URL, 포트</div>
+                </div>
+            </a>
+        </li>
+
     </ul>
 </div>
 
