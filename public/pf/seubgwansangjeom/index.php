f3ebf5ef98ee81474e29399ba71fa691b6a70ceb
Bea-sooyeon <qotndus5@naver.com>
2020-07-09 18:23:14 +0900

    습관상점 슬라이더

--- public/pf/seubgwansangjeom/index.php
+++ public/pf/seubgwansangjeom/index.php
@@ -1,20 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="./index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    <script src="./index.js"></script>
 </head>
+
 <body>
 
-    <div class="top-line flex flex-ai-c flex-jc-c">[NEW] 습관상점 프리미엄 훈제연어 슬라이스 출시!!</div>
+    <div class="top-line flex flex-ai-c flex-jc-c">
+        [NEW] 습관상점 프리미엄 훈제연어 슬라이스 출시!!
+    </div>
 
     <div class="top-bar flex">
 
         <div class="logo flex">
-            <img src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/bf86a0bfdc3224b6a49b5e003fd73bb1_3_top.jpg" alt="">
+            <img src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/bf86a0bfdc3224b6a49b5e003fd73bb1_3_top.jpg"
+                alt="">
         </div>
 
         <div class="menu-bar flex-ai-c flex-jc-c flex flex-grow-1">
@@ -40,36 +48,39 @@
         <div class="right-box flex flex-jc-c flex-ai-c">
             <li class="login font-1"><a href="#">LOGIN</a></li>
             <li class="join font-1"><a href="#">JOIN</a></li>
-            <li class="my-page"><a href="#"><img src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/icon_mypage.png" alt=""></a></li>
-            <li class="cart"><a href="#"><img src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/icon_cart.png" alt=""</a></li>
-            <li class="search"><a href="#"><img src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/icon_search.png" alt=""></a></li>
+            <li class="my-page"><a href="#"><img
+                        src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/icon_mypage.png" alt=""></a></li>
+            <li class="cart"><a href="#"><img src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/icon_cart.png"
+                        alt="" </a> </li> <li class="search"><a href="#"><img
+                            src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/icon_search.png" alt=""></a>
+            </li>
         </div>
     </div>
 
-    <div class="bn-box">
-        <div class="img-box">
-            <img src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/yg_bn_1_01.jpg" alt="">
-        </div>    
+    <div class="slider">
+
+        <div class="slides">
+            <div class="active"
+                style="background-image:url(https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/yg_bn_1_01.jpg);">
+            </div>
+
+            <div style="background-image:url(https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/yg_bn_1_02.jpg);">
+            </div>
+
+            <div style="background-image:url(https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/yg_bn_1_03.jpg);">
+            </div>
+        </div>
+
+        <div class="dots">
+            <ul class="flex">
+                <li class="active"></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+
     </div>
 
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quos optio unde eligendi? Consequuntur iure optio temporibus veniam quia. Dolorem accusantium suscipit et itaque voluptate aut iste ducimus modi delectus!
-    Ea, quod. Sit, excepturi? Fuga, ipsum quasi nemo a laborum, neque aspernatur architecto perspiciatis aliquid non iusto sit facere! Hic praesentium ex molestias libero doloremque est ipsa ea id quaerat?
-    Fuga expedita doloribus corrupti pariatur atque culpa necessitatibus molestiae nostrum facere. Maiores, odio sint aliquam, perferendis qui blanditiis inventore nulla repellat recusandae voluptate, beatae veritatis eaque quis cumque impedit nisi?
-    Porro perferendis odit dignissimos, iste beatae atque, asperiores excepturi blanditiis omnis ipsum voluptates neque saepe animi commodi obcaecati aperiam odio qui? Harum nemo numquam voluptatem iste ipsa cupiditate qui consequuntur?
-    Deserunt sapiente nam, debitis nostrum qui consequuntur maiores, veniam ullam, modi optio possimus ut quaerat. Maiores consequatur aliquid voluptatum, magnam quia similique natus ut libero vitae ipsa dignissimos, eveniet tempore.
-    Facilis libero, accusamus in laboriosam quibusdam perferendis aspernatur possimus. Nostrum, aspernatur! Adipisci nulla rem nihil illo dignissimos repellat culpa? Facilis blanditiis quibusdam possimus nulla esse velit! Deleniti ab fugiat nobis.
-    Obcaecati expedita earum ullam voluptates placeat voluptas eius dignissimos ducimus possimus repellat. Modi voluptates corporis porro magnam a! Repellendus architecto, saepe officia voluptate nostrum perspiciatis laudantium enim molestiae fuga magni.
-    Laboriosam rerum autem harum, error ipsum vero corrupti voluptatum veniam consequuntur excepturi porro, voluptatem dicta nesciunt reprehenderit distinctio enim corporis modi! Laboriosam repudiandae esse corrupti atque doloremque consectetur incidunt exercitationem?
-    Quo eos omnis repudiandae laborum. Consectetur alias qui optio repellat est vitae totam, nostrum animi, iure provident blanditiis ipsum laboriosam ut quasi rem eveniet possimus eaque ex hic exercitationem. Repudiandae.
-    Non, pariatur obcaecati culpa repellendus eius consequatur. Laudantium asperiores itaque alias voluptatibus. Enim hic voluptate esse eos libero veritatis reprehenderit quibusdam provident corrupti assumenda itaque, cumque ipsam voluptates temporibus nemo?
-    Explicabo officia error, velit illum ex temporibus quaerat accusantium eum minus. Nemo quae praesentium esse aspernatur itaque nulla! Praesentium facilis inventore ad tempora harum nam fugiat et a, quod iure.
-    Adipisci, molestias mollitia? Unde illo est aspernatur laboriosam doloribus neque quia reprehenderit, minus nisi, ex dolor cumque rerum numquam? Veritatis omnis tempora doloribus, possimus voluptates nesciunt nobis saepe porro delectus.
-    Molestiae unde praesentium repellendus quod, pariatur error nihil reprehenderit ipsum eum voluptas tempore sequi, repudiandae rem quae. Esse, quisquam pariatur iure, suscipit doloremque voluptate repudiandae ea amet quas laborum sit?
-    Sint, explicabo ab beatae omnis in magnam nam. Beatae placeat, quia aperiam ad, libero unde commodi accusamus dolores explicabo neque corporis nostrum vitae reiciendis tenetur nisi alias dolore deleniti accusantium!
-    Optio est illum quibusdam magnam distinctio quidem laborum odio facilis voluptate architecto dolorum iusto, numquam veniam quos ipsa temporibus quis maiores tempore ad officia nemo fugiat repellat! Necessitatibus, quisquam ex.
-
-
 </body>
+
 </html>
-
-
