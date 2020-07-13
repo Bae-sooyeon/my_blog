9cdfb41bd4fa290ce47ce3eba16f17fee47348f8
Bae-sooyeon <qotndus5@naver.com>
2020-07-13 18:37:02 +0900

    footer 적용

--- public/pf/covernot/index.php
+++ public/pf/covernot/index.php
@@ -249,15 +249,16 @@
     <div class="mid-bn-img con flex">
         <div class="img-box">
             <img src="https://covernat.net/SkinImg/img/main1.jpg" alt="">
-                <div class="text">커버낫 X 처키</div>
+            <div class="text">커버낫X처키</div>
         </div>
         <div class="img-box">
             <img src="https://covernat.net/SkinImg/img/main2.jpg" alt="">
-        </div>     
+            <div class="text">AUTHENTIC LOGO TEE</div>
+        </div>
     </div>
 
 
-    
+
     <!-- 신상품 -->
     <div class="best-product-box text-ai-c">
         <div class="best-product-1">New Arrivals</div>
@@ -269,44 +270,43 @@
     </div>
 
 
-    
+
     <!-- 아이템 리스트 -->
     <div class="prod-list-box con">
         <ul class="flex flex-wrap">
             <li>
                 <a href="#">
                     <div class="img-box"><img
-                            src="https://covernat.net/web/product/small/20200622/72c50b78fbe1e26fac33dd1586203d87.jpg"
+                            src="https://bae-sooyeon.github.io/img1/pf/covernot/3b96769cdd8f03d74189935c4775832d.jpg"
                             alt=""></div>
-                    <div class="prod-name">쿨 코튼 스위밍 풀 티셔츠 화이트</div>
-                    <div class="prod-price">39,000원</div>
+                    <div class="prod-name">(MEN) COVERNAT X ONEILL 2020 SKINS RASH GUARD</div>
+                    <div class="prod-price">69,000원</div>
                 </a>
             </li>
             <li>
                 <a href="#">
                     <div class="img-box"><img
-                            src="https://covernat.net/web/product/small/20200428/77ec0cc2f8b2a51b23e034a66057cc3b.jpg"
+                            src="https://bae-sooyeon.github.io/img1/pf/covernot/a236cf5934b5a8909da5faefb519f7af.jpg"
                             alt=""></div>
-                    <div class="prod-name">CXPEANUTS 70th SURFING SNOOPY TEE WHITE</div>
-                    <div class="prod-price"><del>39,000원</del>
-                        <div class="dis">23,400원 40% 할인</div>
+                    <div class="prod-name">타이다잉 에코백</div>
+                    <div class="prod-price">25,000원</div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="img-box"><img
+                            src="https://bae-sooyeon.github.io/img1/pf/covernot/8f727fae12cd7bc6b80ca437d161b633.jpg"
+                            alt=""></div>
+                    <div class="prod-name">쿨 린넨 어센틱 로고 티셔츠 화이트</div>
+                    <div class="prod-price"><del>45,000원</del>
+                        <div class="dis">38,250원 15% 할인</div>
                     </div>
                 </a>
             </li>
             <li>
                 <a href="#">
                     <div class="img-box"><img
-                            src="https://covernat.net/web/product/small/202007/260fdc685b6720d9165881a46917803f.jpg"
-                            alt=""></div>
-                    <div class="prod-name">AUTHENTIC LOGO AUTOMATIC UMBRELLA BLACK</div>
-                    <div class="prod-price">15,900원<div class="dis">13,510원 15.03% 할인</div>
-                    </div>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <div class="img-box"><img
-                            src="https://covernat.net/web/product/small/20200616/2e66e9e06ef8c986b4efedb3b0c445ec.jpg"
+                            src="https://bae-sooyeon.github.io/img1/pf/covernot/da5afb16be640bb89b79c0c6ea7a78a3.jpg"
                             alt=""></div>
                     <div class="prod-name">캡슐 로고 티셔츠 블루</div>
                     <div class="prod-price">39,000원</div>
@@ -315,27 +315,25 @@
             <li>
                 <a href="#">
                     <div class="img-box"><img
-                            src="https://covernat.net/web/product/small/20200406/2eb0fc4f78f5519d0dc7b838aafca6ef.jpg"
+                            src="https://bae-sooyeon.github.io/img1/pf/covernot/fc8c9b791a4495e690c8dfb76c12c618.jpg"
                             alt=""></div>
-                    <div class="prod-name">어센틱 로고 티셔츠 빈티지 그린</div>
-                    <div class="prod-price">39,000원<div class="dis">27,300원 30% 할인</div>
-                    </div>
+                    <div class="prod-name">쿨 코튼 서핑 트립 티셔츠 화이트</div>
+                    <div class="prod-price">39,000원</div>
                 </a>
             </li>
             <li>
                 <a href="#">
                     <div class="img-box"><img
-                            src="https://covernat.net/web/product/small/20200410/fcc8d8675fcdf915101c3f81331d0799.jpg"
+                            src="https://bae-sooyeon.github.io/img1/pf/covernot/f937df6a68161bffb60e6eff4e67880c.jpg"
                             alt=""></div>
-                    <div class="prod-name">[레드벨벳 예리]서퍼맨 티셔츠 블랙</div>
-                    <div class="prod-price">39,000원<div class="dis">23,400원 40% 할인</div>
-                    </div>
+                    <div class="prod-name">쿨 코튼 스위밍 풀 티셔츠 블랙</div>
+                    <div class="prod-price">39,000원</div>
                 </a>
             </li>
             <li>
                 <a href="#">
                     <div class="img-box"><img
-                            src="https://covernat.net/web/product/small/20200623/4d00cecf33825446c192886f4183a3f0.jpg"
+                            src="https://bae-sooyeon.github.io/img1/pf/covernot/9c61ba9949117282d5af3a0787be022c.jpg"
                             alt=""></div>
                     <div class="prod-name">S/S ARCH LOGO TEE WHITE/RED</div>
                     <div class="prod-price">39,000원<div class="dis">27,300원 30% 할인</div>
@@ -345,7 +343,7 @@
             <li>
                 <a href="#">
                     <div class="img-box"><img
-                            src="https://covernat.net/web/product/small/20200428/b7f8ff418c4d50e56c37871a4a63d7e2.jpg"
+                            src="https://bae-sooyeon.github.io/img1/pf/covernot/f857d25ee4d827a68ed4719932938c2c.jpg"
                             alt=""></div>
                     <div class="prod-name">CXPEANUTS 70th PADDLING SNOOPY TEE BLACK</div>
                     <div class="prod-price">39,000원<div class="dis">27,300원 30% 할인</div>
@@ -355,6 +353,72 @@
         </ul>
     </div>
 
+
+
+    <!-- 중하단 배너 -->
+    <div class="lower-bn con">
+        <div class="img-box">
+            <img src="https://covernat.net/SkinImg/img/main3.jpg" alt="">
+            <div class="text">COVERNAT X SNOOPY<div class="text-2">2020 스누피 콜라보레이션</div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="lower-bn-2 con">
+        <div class="img-box">
+            <img src="https://covernat.net/SkinImg/img/instagram_main1.gif" alt="">
+            <div class="text-1">@covernat</div>
+        </div>
+    </div>
+
+    <div class="footer-line"></div>
+
+    <div class="footer flex">
+        <div class="footer-small-left flex flex-1-0-0">
+            <ul class="information">information
+                <li>About</li>
+                <li>매장 안내</li>
+                <li>이용약관</li>
+                <li>개인정보취급방침</li>
+            </ul>
+            <ul class="Customer Service">Customer Service
+                <li>문의</li>
+                <li>배송</li>
+                <li>교환 및 반품</li>
+                <li>주문 조회</li>
+            </ul>
+            <ul class="Social">Social
+                <li>Instagram</li>
+                <li>Facebook</li>
+                <li>Blog</li>
+                <li>Kakaotalk</li>
+            </ul>
+            <ul class="02-338-3316">02-338-3316
+                <li>평일 오전 9시 - 오후 6시</li>
+                <li>점심시간 오후 1시 - 2시</li>
+                <li>주말, 공휴일 휴무</li>
+            </ul>
+        </div>
+
+        <div class="footer-small-right-box flex flex-1-0-0">
+            <div class="footer-small-right">
+                <div>(주)배럴즈</div><br>
+                <div>서울 강남구 논현로 159길 52 [52, Nonhyeon-ro 159-gil, Gangnam-gu, Seoul, Korea]
+                    대표. 윤형석 [Yun, Hyungseok] . 사업자 등록번호. 261-81-17293 . 통신판매업. 신고번호 제 2014-서울강남-02931호
+                    개인정보관리책임자 . 진수원 [Jin_sw@barrels.co.kr] . TEL. 02-338-3316 .
+                </div><br>
+                <div>
+                    Copylight(c) 2008 COVERNAT. All Right Reserved.
+                </div>
+            </div>
+        </div>
+
+
+
+
+    </div>
 
 
 </body>
