650e44185b38ccc5bf0f209a761be5b62e3ad853
Bae-sooyeon <qotndus5@naver.com>
2020-07-14 18:45:56 +0900

    tablier-food 구현

---
+++ public/pf/tablier-food.jp/index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="./index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    <script src="./index.js"></script>
+</head>
+
+<body>
+
+    <div class="menu-bar flex">
+        <div class="logo flex flex-ai-c flex-jc-c">
+            <img src="https://bae-sooyeon.github.io/img1/pf/tablier/logo.png" alt="">
+        </div>
+
+        <div class="menu-box-1 flex-1-0-0 flex-jc-c flex-ai-c">
+            <ul class= "flex">
+                <li><a href="">Top</a></li>
+                <li><a href="">Policy</a></li>
+                <li><a href="">Business</a></li>
+                <li><a href="">Works</a></li>
+                <li><a href="">Staff</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">Company</a></li>
+                <li><a href="">School</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </div>
+        
+        <div class="menu-right-box flex flex-jc-c flex-ai-c">
+            <a href="#"><img src="https://bae-sooyeon.github.io/img1/pf/tablier/sns_fb.png" alt=""></a>
+            <a href="#"><img src="https://bae-sooyeon.github.io/img1/pf/tablier/sns_ist.png" alt=""></a>
+        </div>
+    </div>
+
+    
+    <!-- 슬라이더 배너 -->
+
+    <div class="slider">
+        <div class="slides">
+            <div class="active"
+                style="background-image:url(https://bae-sooyeon.github.io/img1/pf/tablier/top_img.jpg);">
+            </div>
+
+            <div style="background-image:url(https://bae-sooyeon.github.io/img1/pf/tablier/top_img2.jpg);">
+            </div>
+
+            <div style="background-image:url(https://bae-sooyeon.github.io/img1/pf/tablier/top_img3.jpg);">
+            </div>
+        </div>
+
+        <div class="dots">
+            <ul>
+                <li class="active"></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+
+        <div class="bn-cover-img">
+            <img src="https://bae-sooyeon.github.io/img1/pf/tablier/top_copy1-1.svg" alt="">
+        </div>
+    </div>
+
+    <!-- 중간 배너 박스 -->
+    <div class="mid-bn-box flex flex-jc-c">
+        <img src="https://bae-sooyeon.github.io/img1/pf/tablier/top_copy2-1.svg" alt="">
+        <img src="https://bae-sooyeon.github.io/img1/pf/tablier/top_copy2-2.svg" alt="">
+    </div>
+
+    <div class="small-title">
+            豊かなテーブルは幸せな心と暮らしをつくる
+        </div>
+
+    <div class="small-title-2">
+        フードオフィスタブリエは、名古屋を拠点にメニュー・レシピ・企画開発、<br>
+飲食店舗メニューコーディネート等を行っています。<br>
+豊かなテーブルを彩るため「食」への思いをカタチにして届ける、<br>
+フードコーディネーター・フードスタイリストの職人集団です。
+    </div>    
+
+    <div class="img-box">
+        <div class="red-fr">
+            <img src="https://bae-sooyeon.github.io/img1/pf/tablier/top_photocut2.png" alt="">
+        </div>
+        
+        <div class="pasta">
+            <img src="https://bae-sooyeon.github.io/img1/pf/tablier/top_photocut1.png" alt="">
+        </div>
+    </div>
+    <!-- 중간 배너 박스 끝-->
+
+    <!-- 중간 이미 슬라이더 -->
+    <div class="mid-img-slider">
+        <img src="https://bae-sooyeon.github.io/img1/pf/tablier/top_subimg1.jpg" alt="">
+        <img src="https://bae-sooyeon.github.io/img1/pf/tablier/top_subimg2.jpg" alt="">
+        <img src="https://bae-sooyeon.github.io/img1/pf/tablier/top_subimg3.jpg" alt="">
+        <img src="https://bae-sooyeon.github.io/img1/pf/tablier/top_subimg4.jpg" alt="">
+        <img src="https://bae-sooyeon.github.io/img1/pf/tablier/top_subimg5.jpg" alt="">
+    </div>
+
+    <!-- About 버튼 -->
+    <div class="about-bt flex flex-ai-c flex-jc-c">
+        <a href="#">About TABLIER</a>
+    </div>
+
+    <!-- 중간 소배너 -->
+    <div class="mid-sm-bn">
+        <div class="img-box">
+            <img src="https://bae-sooyeon.github.io/img1/pf/tablier/bnr_school.jpg" alt="">
+        </div>
+    </div>
+
+
+</body>
+
+</html>
