771305113bcacf611a58df22aa05d7a5b62c9a04
Bae-sooyeon <qotndus5@naver.com>
2020-07-16 17:10:42 +0900

    습관상점 수정

--- public/pf/seubgwansangjeom/index.php
+++ public/pf/seubgwansangjeom/index.php
@@ -146,16 +146,16 @@
 
     <!-- 아이템 리스트 박스 -->
     <div class="prod-list-box con">
-        <ul class="flex">
+        <ul class="flex flex-wrap-wrap">
             <li>
                 <a href="#">
                     <div class="img-box"><img
                             src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/451266b96c9495284d98b424213fa956.jpg"
                             alt=""></div>
                 </a>
-                <div class="prod-name">이름</div>
-                <div class="prod-price">가격</div>
-                <div class="prod-ex">설명</div>
+                <div class="prod-name">습관상점 프리미엄 훈제연어 슬라이스 300g 다이어트 홈파티 슈퍼푸드 연어회</div>
+                <div class="prod-price">14,900원</div>
+                <div class="prod-ex">더 건강한 먹거리 습관! 다이어트식단, 홈파티, 혼술안주로도 굳!</div>
             </li>
             <li>
                 <a href="#">
@@ -163,9 +163,9 @@
                             src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/caf726a60a593f45bca21d662061da8d.jpg"
                             alt=""></div>
                 </a>
-                <div class="prod-name">이름</div>
-                <div class="prod-price">가격</div>
-                <div class="prod-ex">설명</div>
+                <div class="prod-name">[5월출시예정] 습관상점 프리미엄 훈제연어 슬라이스 120g</div>
+                <div class="prod-price">COMING SOON</div>
+                <div class="prod-ex">편리한 소포장! 더 건강한 먹거리 습관! 다이어트식단, 홈파티, 혼술안주로도 굳!</div>
             </li>
             <li>
                 <a href="#">
@@ -173,20 +173,19 @@
                             src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/fc43b7d109fd85cbd31b6d55e2212ab5.jpg"
                             alt=""></div>
                 </a>
-                <div class="prod-name">이름</div>
-                <div class="prod-price">가격</div>
-                <div class="prod-ex">설명</div>
+                <div class="prod-name">하루한포 습관상점 빠나나푸룬즙 푸룬주스 7개입</div>
+                <div class="prod-price"><del>13,500원</del><br>10,900원</div>
+                <div class="prod-ex">가뿐한 화장실 습관! 임산부, 어린이도 맛있게 먹을 수 있는 푸룬주스!</div>
             </li>
-
             <li>
                 <a href="#">
                     <div class="img-box"><img
                             src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/76d96c1dbe6c81426bea6104cf4a78d0.jpg"
                             alt=""></div>
                 </a>
-                <div class="prod-name">이름</div>
-                <div class="prod-price">가격</div>
-                <div class="prod-ex">설명</div>
+                <div class="prod-name">하루한포 습관상점 빠나나푸룬즙 푸룬주스 24개입</div>
+                <div class="prod-price"><del>44,500원</del><br>29,900원</div>
+                <div class="prod-ex">가뿐한 화장실 습관! 선물용으로 Good!</div>
             </li>
             <li>
                 <a href="#">
@@ -194,9 +193,9 @@
                             src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/98816a7e63f7bb3e0ffb6c1a07d77c64.jpg"
                             alt=""></div>
                 </a>
-                <div class="prod-name">이름</div>
-                <div class="prod-price">가격</div>
-                <div class="prod-ex">설명</div>
+                <div class="prod-name">하루한포 습관상점 빠나나푸룬즙 푸룬주스 28개입(+14포 추가 증정)</div>
+                <div class="prod-price"><del>44,500원</del><br>29,900원</div>
+                <div class="prod-ex">14포 추가 증정이벤트! / 1포당 712원꼴!</div>
             </li>
         </ul>
     </div>
@@ -206,15 +205,239 @@
         <iframe width="957" height="538" src="https://www.youtube.com/embed/6BSEeOa1G_4" frameborder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <div class="learn-more-box">
-            <div class="text-1">LEARN MORE</div>    
+            <div class="text-1">LEARN MORE</div>
             <div class="text-2">습관상점이 더 궁금하시다면?</div>
             <div class="text-3">
                 #좋은 건 습관하자 <br>
                 #습관상점
             </div>
-            <div class="text-4"><a href="#">습관상점 인스타그램 바로가기<br></a></div>            
+            <div class="text-4"><a href="#">습관상점 인스타그램 바로가기<br></a></div>
         </div>
     </div>
+
+    <!-- 습관상점이 추천하는 인기 습관 -->
+    <div class="hello-1 text-align-center">
+        습관상점이 추천하는
+    </div>
+    <div class="hello-2 text-align-center">
+        인기 습관
+    </div>
+
+    <!-- 아이템 리스트 박스 -->
+    <div class="prod-list-box con">
+        <ul class="flex flex-wrap-wrap">
+            <li>
+                <a href="#">
+                    <div class="img-box"><img
+                            src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/451266b96c9495284d98b424213fa956.jpg"
+                            alt=""></div>
+                </a>
+                <div class="prod-name">습관상점 프리미엄 훈제연어 슬라이스 300g 다이어트 홈파티 슈퍼푸드 연어회</div>
+                <div class="prod-price">14,900원</div>
+                <div class="prod-ex">더 건강한 먹거리 습관! 다이어트식단, 홈파티, 혼술안주로도 굳!</div>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="img-box"><img
+                            src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/caf726a60a593f45bca21d662061da8d.jpg"
+                            alt=""></div>
+                </a>
+                <div class="prod-name">[5월출시예정] 습관상점 프리미엄 훈제연어 슬라이스 120g</div>
+                <div class="prod-price">COMING SOON</div>
+                <div class="prod-ex">편리한 소포장! 더 건강한 먹거리 습관! 다이어트식단, 홈파티, 혼술안주로도 굳!</div>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="img-box"><img
+                            src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/fc43b7d109fd85cbd31b6d55e2212ab5.jpg"
+                            alt=""></div>
+                </a>
+                <div class="prod-name">하루한포 습관상점 빠나나푸룬즙 푸룬주스 7개입</div>
+                <div class="prod-price"><del>13,500원</del><br>10,900원</div>
+                <div class="prod-ex">가뿐한 화장실 습관! 임산부, 어린이도 맛있게 먹을 수 있는 푸룬주스!</div>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="img-box"><img
+                            src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/76d96c1dbe6c81426bea6104cf4a78d0.jpg"
+                            alt=""></div>
+                </a>
+                <div class="prod-name">하루한포 습관상점 빠나나푸룬즙 푸룬주스 24개입</div>
+                <div class="prod-price"><del>44,500원</del><br>29,900원</div>
+                <div class="prod-ex">가뿐한 화장실 습관! 선물용으로 Good!</div>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="img-box"><img
+                            src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/98816a7e63f7bb3e0ffb6c1a07d77c64.jpg"
+                            alt=""></div>
+                </a>
+                <div class="prod-name">하루한포 습관상점 빠나나푸룬즙 푸룬주스 28개입(+14포 추가 증정)</div>
+                <div class="prod-price"><del>44,500원</del><br>29,900원</div>
+                <div class="prod-ex">14포 추가 증정이벤트! / 1포당 712원꼴!</div>
+            </li>
+        </ul>
+    </div>
+
+    <!-- 여러분의 습관상점, 어떠셨나요?  -->
+    <div class="hello-3 text-align-center">
+        상점 후기
+    </div>
+    <div class="hello-4 text-align-center">
+        여러분의 습관상점, 어떠셨나요? <br>
+        더 많은 후기 보기
+    </div>
+
+    <!-- 사진 리뷰  -->
+    <div class="photo-review-list-box con">
+        <ul class="flex flex-wrap-wrap">
+            <li>
+                <a href="#">
+                    <div class="img-box"><img
+                            src="https://habitloom.com/file_data/habitloom//2020/07/12/cb5a7aae779e28be3a290b0cc4481fcf.jpeg"
+                            alt=""></div>
+                </a>
+                <div class="review-box">
+                    <div class="review-item">습관상점 프리미엄 훈제연어 슬라이스 300...</div>
+                    <div class="review-ex">비린내 없이 양도 많고 느므 맛나게 먹었습니다 :)</div>
+                </div>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="img-box"><img
+                            src="https://habitloom.com/file_data/habitloom//2020/06/11/45ab1ff4f79247c9019449e1388b8c00.jpg"
+                            alt=""></div>
+                </a>
+                <div class="review-box">
+                    <div class="review-item">하루한포 습관상점 빠나나푸룬즙 푸룬주스 2...</div>
+                    <div class="review-ex">변비있는 아들 푸룬열매로도 먹기 힘들</div>
+                </div>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="img-box"><img
+                            src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/f8cfeb2dd4ddf6a85afb6257194efaca.jpg"
+                            alt=""></div>
+                </a>
+                <div class="review-box">
+                    <div class="review-item">하루한포 습관상점 빠나나푸룬즙 푸룬주스 2...</div>
+                    <div class="review-ex">아이가 변비가 갑자기 와서 몇일을 힘</div>
+                </div>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="img-box"><img
+                            src="https://habitloom.com/file_data/habitloom//2020/05/15/ab0c0353f6c12e7a05e1ac8f5cf593de.jpeg"
+                            alt=""></div>
+                </a>
+                <div class="review-box">
+                    <div class="review-item">습관상점 프리미엄 훈제연어 슬라이스 300...</div>
+                    <div class="review-ex">잘받았습니다!!!!</div>
+                </div>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="img-box"><img
+                            src="https://habitloom.com/file_data/habitloom//2020/05/14/608ff3f9aa349f68fa3d4fba7e94417f.jpg"
+                            alt=""></div>
+                </a>
+                <div class="review-box">
+                    <div class="review-item">하루한포 습관상점 빠나나푸룬즙 푸룬주스 2...</div>
+                    <div class="review-ex">아직개봉전이지만 기대가 커요!</div>
+                </div>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="img-box"><img
+                            src="https://habitloom.com/file_data/habitloom//2020/04/28/47b7d5eb5d9e0fbd03b2b6fcfd42ecf9.jpg"
+                            alt=""></div>
+                </a>
+                <div class="review-box">
+                    <div class="review-item">습관상점 프리미엄 훈제연어 슬라이스 300...</div>
+                    <div class="review-ex">와인 혼술안주로 최고에요~</div>
+                </div>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="img-box"><img
+                            src="https://habitloom.com/file_data/habitloom//2020/04/22/00e7c3cddab1d420ded0549fdb28ddce.jpg"
+                            alt=""></div>
+                </a>
+                <div class="review-box">
+                    <div class="review-item">습관상점 프리미엄 훈제연어 슬라이스 300...</div>
+                    <div class="review-ex">식구가 많아서 5팩 구매했어요~ㅎㅎ
+                        훈제연어는 거기서 거기라고 생</div>
+                </div>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="img-box"><img
+                            src="https://habitloom.com/file_data/habitloom//2020/04/21/47fc62d996e6a86c2384910eb0b642e5.jpg"
+                            alt=""></div>
+                </a>
+                <div class="review-box">
+                    <div class="review-item">습관상점 프리미엄 훈제연어 슬라이스 300...</div>
+                    <div class="review-ex">5팩구매해서 오늘 받자마자 벌써 한팩</div>
+                </div>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="img-box"><img
+                            src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/f1a5f969647e3fe284832b24e7ff5bfa.jpg"
+                            alt=""></div>
+                </a>
+                <div class="review-box">
+                    <div class="review-item">습관상점 프리미엄 훈제연어 슬라이스 300...</div>
+                    <div class="review-ex">아들램이 며칠 전부터 계속 부페가자고</div>
+                </div>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="img-box"><img
+                            src="https://habitloom.com/file_data/habitloom//2020/04/17/3e7b4f29422ba2e8caf224fe05da2de4.jpg"
+                            alt=""></div>
+                </a>
+                <div class="review-box">
+                    <div class="review-item">습관상점 프리미엄 훈제연어 슬라이스 300...</div>
+                    <div class="review-ex">짜지 않고 맛있어요 같이 먹은 친구들도 다들 짜지 않아 좋다고 하네요~</div>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+    <!-- 하단 배너 -->
+    <div class="foot-bn con">
+        <div class="img-box">
+            <img src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/yg_bn_4_01.jpg" alt="">
+        </div>
+    </div>
+
+    <!-- footer 라인 -->
+    <div class="footer-line"></div>
+
+    <!-- footer -->
+    <ul class="footer-box">
+        <div class="footer-1 font-1">
+            BRAND STORY <div class="stick inline-block">|</div> AGREEMENT <div class="stick inline-block">|</div>
+            PRIVACY POLICY <div class="stick inline-block">|</div> GUIDE
+        </div>
+        <ul class="footer-2">
+            <li>(주)에스피에스 인터네셔널</li>
+            <li>OWNER 신동환 ADDRESS (06249) 서울특별시 강남구 논현로71길 25 2층 (역삼동) BUSINESS LICENSE 139-81-65765</li>
+            <li>MAIL ORDER LICENSE 제2019-서울강남-05067 [사업자정보확인]</li>
+            <li>CS CENTER 070-8286-9801 (AM 10:00 - PM 18:00 / LUNCH PM 12 - PM 1 / SAT.SUN.HOLIDAY OFF)</li>
+            <li>BANK INFO 국민 851401-00-037418 / 예금주 (주)에스피에스 인터네셔널</li>
+        </ul>    
+    </div>
+    <div class="right-footer">
+            <ul class="font-1">
+                <li><a href="#">INSTAGRAM</a></li>
+                <li><a href="#">FACEBOOK</a></li>
+                <li><a href="#"><img src="https://bae-sooyeon.github.io/img1/pf/seubqwansangjeom/escrow_43x43_color.png" alt=""></a></li>
+                <li>Copyright © 습관상점 All rights reserved</li>
+            </ul>
+        </div>
+
 
 </body>
 
