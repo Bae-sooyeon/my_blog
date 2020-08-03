530dc98113473ec2800027c2dd3131905da9682e
Bae-sooyeon <qotndus5@naver.com>
2020-08-03 18:34:35 +0900

    카카오 추가 작업

--- public/pf/kakaopaysec/index.php
+++ public/pf/kakaopaysec/index.php
@@ -14,7 +14,7 @@
 <body>
     <!-- 탑바 시작-->
     <div class="top-bar">
-        <div class="con h-100p flex flex-sp-bt">
+        <div class="con h-100p flex flex-sp-b">
             <a href="#" class="logo flex flex-ai-c">
                 <img src="https://bae-sooyeon.github.io/img1/pf/kakaopaysec/bi.png" alt="">
             </a>
@@ -73,43 +73,77 @@
             <img src="https://bae-sooyeon.github.io/img1/pf/kakaopaysec/main-visual01.jpg" alt="">
         </div>
         <div class="bn-img-text-wrap">
-            <div class="bn-img-text">카카오페이증권</div>
-            <div class="bn-img-text-2">플랫폼을 통한 새로운 투자문화를 만듭니다.</div>
-            <div class="bn-img-text-3">KAKAOPAY</div>
-            <div class="bn-img-text-4">SECURITIES</div>
+            <div class="bn-img-text fadeInUp active-on-visible" style="visibility:visible;">카카오페이증권</div>
+            <div class="bn-img-text-2 fadeInUp active-on-visible" style="animation-delay: 0.2s;">플랫폼을 통한 새로운 투자문화를 만듭니다.</div>
+            <div class="bn-img-text-3 fadeInLeft active-on-visible" style="animation-delay: 0.8s;">KAKAOPAY</div>
+            <div class="bn-img-text-4 fadeInLeft active-on-visible"style="animation-delay:1s;">SECURITIES</div>
         </div>
     </div>
 
     <!-- 마음놓고 투자하다 시작-->
-    <div class="main-company-info">
-        <div class="main-text-1">
+    <div class="main-company-info fadeInLeft2 active-on-visible" data-active-on-visible-diff-y="-200">
+        <div class="main-text-1 fadeInUp active-on-visible">
             마음 놓고 투자하다
         </div>
-        <div class="main-text-2">
-        누구든지 정보나 자산 규모에 따른 차별 없이<br>
-        편리하고 안전하게 자산을 관리할 수 있도록<br>
-        플랫폼을 통한 새로운 투자 문화를 만들어 갑니다.
+        <div class="main-text-2 fadeInUp active-on-visible">
+            누구든지 정보나 자산 규모에 따른 차별 없이<br>
+            편리하고 안전하게 자산을 관리할 수 있도록<br>
+            플랫폼을 통한 새로운 투자 문화를 만들어 갑니다.
         </div>
-        <div class="main-bt">
+        <div class="main-bt fadeInUp active-on-visible">
             <a href="#">회사 소개</a>
         </div>
     </div>
 
     <!-- 카카오페이 증권 인재 시작-->
-    <div class="inner-wrap">
-        <div class="inner-content-box flex">
-            <div class="inner-content-1">
-            카카오페이증권
+    <div class="inner-wrap-box">
+        <div class="img-box active-on-visible">
+            <img src="https://bae-sooyeon.github.io/img1/pf/kakaopaysec/main-recruit-bg.jpg" alt="">
+        </div>
+        <div class="inner-content-box flex fadeInUp active-on-visible">
+            <div class="inner-text-1">
+                카카오페이증권
             </div>
-            <div class="inner-content-2">
-            인재모집
+            <div class="inner-text-2">
+                인재모집
             </div>
-            <div class="main-bt">
-                <a href="#">채용 정보</a>
+        </div>
+        <div class="main-bt fadeInUp active-on-visible">
+            <a href="#">회사 소개</a>
+        </div>
+    </div>
+
+    <!-- footer 시작-->
+    <div class="footer-wrap-box">
+        <div class="footer con">
+            <div class="footer-menu">
+                <ul class="flex">
+                    <li><a href="#">이용약관</a></li>
+                    <li><a href="#">보호금융상품등록부</a></li>
+                    <li><a href="#">개인정보취급처리방침</a></li>
+                    <li><a href="#">고객권리안내문</a></li>
+                    <li><a href="#">신용정보 활용체제 공시</a></li>
+                </ul>
             </div>
-            <div class="bg-img">
-                
+            <div class="footer-logo con">
+                <img src="https://bae-sooyeon.github.io/img1/pf/kakaopaysec/bi-footer.png" alt="">
             </div>
+            <div class="footer-con-box flex flex-sp-b flex-ai-end">
+                <div class="flex-con-box-2 flex">
+                    <div class="footer-con-1">
+                        경기도 성남시 분당구 판교역로 146번길 20 Office H 12층 <br>
+                        강남오피스: 서울시 강남구 테헤란로 512 신안빌딩 3층 <br>
+                        여의도오피스: 서울시 영등포구 의사당대로1길 34
+                    </div>
+                    <div class="footer-con-2">
+                        고객센터:1600-8515
+                    </div>
+                </div>
+                <div class="footer-con-3">
+                    © 2020 Copyright kakaopay securities corp.
+                </div>
+            </div>
+
         </div>
     </div>
 
