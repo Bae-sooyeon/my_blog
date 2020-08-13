306cd3035bd3892edb5a6bc94a43c8fe70316291
Bae-sooyeon <qotndus5@naver.com>
2020-08-13 18:28:42 +0900

    인사이드 아웃 구현

---
+++ public/pf/insideout/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://rawgit.com/alvarotrigo/fullPage.js/dev/src/fullpage.js"></script>
+    <link rel="stylesheet" href="./index.css">
+    <script src="./index.js"></script>
+</head>
+
+<body>
+
+    <div id="fullpage">
+        <div class="home section">
+            <!-- 배경 -->
+            <div class="bg">
+                <img class="bn-img" src=https://bae-sooyeon.github.io/img1/pf/insideout/bn-1.jpg alt="">
+            </div>
+            <!-- 탑바 -->
+            <div class="top-bar flex-jc-c">
+                <ul class="flex">
+                    <li><a href="#">STORY</a></li>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">TRAILER</a></li>
+                    <li><a href="#">GALLERY</a></li>
+                </ul>
+            </div>
+            <!-- 인사이드 아웃 로고 -->
+            <div class="bn-logo">
+                <img class="inside block fadeInLeft active-on-visible"
+                    src=https://bae-sooyeon.github.io/img1/pf/insideout/inside.png alt="">
+                <img class="out fadeInLeft active-on-visible" style="animation-delay: .3s;"
+                    src=https://bae-sooyeon.github.io/img1/pf/insideout/out.png alt="">
+            </div>
+        </div>
+
+
+        <div class="story section">
+            <div class="bg">
+                <div class="bg-img"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section">Section 3</div>
+        <div class="section">Section 4</div>
+    </div>
+
+
+</body>
+
+</html>
