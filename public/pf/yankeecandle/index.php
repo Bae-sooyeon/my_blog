4dc0ac5d361366d8c4c710ae404f2ea3596f89da
Bae-sooyeon <qotndus5@naver.com>
2020-09-16 16:36:33 +0900

    양키캔들 리디자인

---
+++ public/pf/yankeecandle/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>양키캔들 리디자인</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="./index.css">
+    <script src="./index.js"></script>
+</head>
+
+<body>
+    <!-- 탑바 -->
+    <div class="top-bar flex flex-sp-b flex-ai-c bd-red">
+        <div class="logo flex-ai-c">
+            <img src="https://bae-sooyeon.github.io/img1/pf/yankeecandle/yklogo.png" alt="">
+        </div>
+        <div class="right-box flex flex-ai-c">
+            <a href="#" class="search-box">
+                <img src="https://bae-sooyeon.github.io/img1/pf/yankeecandle/searchBt.png" alt="">
+            </a>
+            <a href="#" class="ham-bt">
+            햄
+            </a>
+        </div>
+    </div>
+
+    <!-- 메인 배너이미지 -->
+    <div class="main-bn">
+        <div class="img-box">
+            <img src="https://bae-sooyeon.github.io/img1/pf/yankeecandle/blackCherryBn.jpg" alt="">
+        </div>
+    </div>
+    
+
+</body>
+
+</html>
