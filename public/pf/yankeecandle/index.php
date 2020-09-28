a3baf79d2ea8b4b3209c2699a07f5ded93fd047b
Bae-sooyeon <qotndus5@naver.com>
2020-09-28 22:50:04 +0900

    양키캔들

--- public/pf/yankeecandle/index.php
+++ public/pf/yankeecandle/index.php
@@ -13,29 +13,53 @@
 </head>
 
 <body>
-    <!-- 탑바 -->
-    <div class="top-bar flex flex-sp-b flex-ai-c">
-        <div class="logo flex-ai-c">
-            <img src="https://bae-sooyeon.github.io/img1/pf/yankeecandle/yklogo.png" alt="">
+    <!-- 맨위 탑바 -->
+    <div class="top-bar-1 con flex flex-sp-b">
+        <div class="lang-kor">
+            한국/한국어
         </div>
-        <div class="right-box flex flex-ai-c">
-            <a href="#" class="search-box">
-                <img src="https://bae-sooyeon.github.io/img1/pf/yankeecandle/searchBt.png" alt="">
-            </a>
-        </div>
+        <ul class="right-menu flex">
+            <li class="shop-find">
+                <a href="#">매장찾기</a>
+            </li>
+            <li class="buy-bt">
+                <a href="#">구매하기</a>
+            </li>
+            <li class="search-bt">
+                <a class="flex" href="#"><img src="https://bae-sooyeon.github.io/img1/pf/yankeecandle/searchBt.png"
+                        alt=""></a>
+            </li>
+        </ul>
     </div>
 
-    <!-- 사이드바 버튼 -->
-    <div class="btn-toggle-side-bar">
-        <div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
+    <!-- 로고 -->
+    <div class="logo flex-jc-c">
+        <img src="https://bae-sooyeon.github.io/img1/pf/yankeecandle/yklogo.png" alt="">
     </div>
 
-    <!-- 사이드바 -->
-    <div class="side-bar"></div>
+    <!-- 메뉴바 -->
+    <div class="menu-bar con flex-jc-c">
+        <ul class="flex">
+            <li>
+                <a href="#">제품</a>
+                    <ul>
+                        <li><a href="#">캔들</a></li>
+                        <li><a href="#">디퓨저·아로마오일
+                            </a></li>
+                        <li><a href="#">차량용·방향제</a></li>
+                        <li><a href="#">워머·액세서리</a></li>
+                        <li><a href="#">선물세트</a></li>
+                        <li><a href="#">메종베르제</a></li>
+                        <li><a href="#">코스메틱</a></li>
+                    </ul>
+            </li>
+            <li><a href="#">아울렛</a></li>
+            <li><a href="#">매장안내</a></li>
+            <li><a href="#">창업안내</a></li>
+            <li><a href="#">고객센터</a></li>
+        </ul>
+    </div>
+
 
 
 
